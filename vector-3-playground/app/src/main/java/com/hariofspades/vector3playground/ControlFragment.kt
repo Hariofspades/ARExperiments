4addd7d343703f2ba1ec81cb9b1c7dfbb0da3c08
Hari <hariutd@gmail.com>
2018-08-17 14:47:25 +0530

    finishing playground project :yay:

--- vector-3-playground/app/src/main/java/com/hariofspades/vector3playground/ControlFragment.kt
+++ vector-3-playground/app/src/main/java/com/hariofspades/vector3playground/ControlFragment.kt
@@ -1,11 +1,15 @@
 package com.hariofspades.vector3playground
 
 
+import android.content.Context
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.SeekBar
+import android.widget.Toast
+import kotlinx.android.synthetic.main.fragment_control.view.*
 
 
 /**
@@ -15,14 +19,72 @@ import android.view.ViewGroup
 class ControlFragment : Fragment() {
 
     lateinit var rootView: View
+    lateinit var vector3ChangedListener: OnVector3ChangedListener
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         rootView = inflater.inflate(R.layout.fragment_control, container, false)
+        setupSeekBar()
         return rootView
     }
 
+    override fun onAttach(context: Context?) {
+        super.onAttach(context)
+        try {
+            vector3ChangedListener = activity as OnVector3ChangedListener
 
+        } catch (exception: ClassCastException) {
+            throw ClassCastException("${activity.toString()} must implement onVector3ChangedListener")
+        }
+    }
+
+    private fun setupSeekBar() {
+
+        rootView.apply {
+
+            seek_x.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+
+                override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
+                    vector3ChangedListener.onSeekXChanged(getConvertedValue(p1))
+                }
+
+                override fun onStartTrackingTouch(p0: SeekBar?) {}
+                override fun onStopTrackingTouch(p0: SeekBar?) {}
+            })
+
+            seek_y.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+
+                override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
+                    vector3ChangedListener.onSeekYChanged(getConvertedValue(p1))
+                    //Toast.makeText(activity, "Value Y: ${getConvertedValue(p1)}", Toast.LENGTH_LONG).show()
+                }
+
+                override fun onStartTrackingTouch(p0: SeekBar?) {}
+                override fun onStopTrackingTouch(p0: SeekBar?) {}
+            })
+
+            seek_z.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+
+                override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
+                    vector3ChangedListener.onSeekZChanged(getConvertedValue(p1))
+                    //Toast.makeText(activity, "Value Z: ${getConvertedValue(p1)}", Toast.LENGTH_LONG).show()
+                }
+
+                override fun onStartTrackingTouch(p0: SeekBar?) {}
+                override fun onStopTrackingTouch(p0: SeekBar?) {}
+            })
+        }
+    }
+
+    fun getConvertedValue(interval: Int): Float {
+        return 0.1f * interval
+    }
+
+    interface OnVector3ChangedListener {
+        fun onSeekXChanged(value: Float)
+        fun onSeekYChanged(value: Float)
+        fun onSeekZChanged(value: Float)
+    }
 
 }
--- vector-3-playground/app/src/main/java/com/hariofspades/vector3playground/MainActivity.kt
+++ vector-3-playground/app/src/main/java/com/hariofspades/vector3playground/MainActivity.kt
@@ -1,12 +1,101 @@
 package com.hariofspades.vector3playground
 
+import android.graphics.Color
 import android.os.Bundle
 import android.support.v4.app.FragmentActivity
+import android.widget.Toast
+import com.google.ar.core.Anchor
+import com.google.ar.core.HitResult
+import com.google.ar.sceneform.AnchorNode
+import com.google.ar.sceneform.math.Vector3
+import com.google.ar.sceneform.rendering.MaterialFactory
+import com.google.ar.sceneform.rendering.ModelRenderable
+import com.google.ar.sceneform.rendering.ShapeFactory
+import com.google.ar.sceneform.ux.ArFragment
 
-class MainActivity : FragmentActivity() {
+class MainActivity : FragmentActivity(), ControlFragment.OnVector3ChangedListener {
+
+    lateinit var arFragment: ArFragment
+
+    var anchorNode: AnchorNode? = null
+
+    var xCoordinate: Float = 0.2f
+    var yCoordinate: Float = 0.2f
+    var zCoordinate: Float = 0.2f
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupSceneformFragment()
+    }
+
+    private fun setupSceneformFragment() {
+
+        arFragment = supportFragmentManager
+                .findFragmentById(R.id.sceneform_fragment)
+                as ArFragment
+
+        arFragment.setOnTapArPlaneListener { hitResult, plane, motionEvent ->
+            makeCube(hitResult, Color.RED)
+        }
+
+    }
+
+    /**
+     * Constructs cube of radius 1f and at position 0.0f, 0.15f, 0.0f on the plane
+     * Here Vector3 takes up the size - 0.2f, 0.2f, 0.2f
+     * @param hitResult - If the hit result is a plane
+     * @param res - Color
+     */
+    private fun makeCube(hitResult: HitResult, color: Int) {
+        MaterialFactory.makeOpaqueWithColor(this,
+                com.google.ar.sceneform.rendering.Color(color))
+                .thenAccept { material ->
+                    addNodeToScene(arFragment, hitResult.createAnchor(),
+                            ShapeFactory.makeCube(
+                                    Vector3(xCoordinate, yCoordinate, zCoordinate),
+                                    Vector3(0.0f, 0.15f, 0.0f),
+                                    material)
+                    )
+
+                }
+    }
+
+    /**
+     * Adds node to the scene and the object.
+     * @param fragment - sceneform fragment
+     * @param anchor - created anchor at the tapped position
+     * @param modelObject - rendered object
+     */
+    private fun addNodeToScene(fragment: ArFragment, anchor: Anchor, modelObject: ModelRenderable) {
+
+        anchorNode = AnchorNode(anchor).apply {
+            renderable = modelObject
+            setParent(anchorNode)
+        }
+
+        fragment.arSceneView.scene.addChild(anchorNode)
+
+    }
+
+    override fun onSeekXChanged(value: Float) {
+        anchorNode?.let {
+            xCoordinate = value
+            it.localScale = Vector3(Vector3(xCoordinate, yCoordinate, zCoordinate))
+        }
+    }
+
+    override fun onSeekYChanged(value: Float) {
+        anchorNode?.let {
+            yCoordinate = value
+            it.localScale = Vector3(Vector3(xCoordinate, yCoordinate, zCoordinate))
+        }
+    }
+
+    override fun onSeekZChanged(value: Float) {
+        anchorNode?.let {
+            zCoordinate = value
+            it.localScale = Vector3(Vector3(xCoordinate, yCoordinate, zCoordinate))
+        }
     }
 }
